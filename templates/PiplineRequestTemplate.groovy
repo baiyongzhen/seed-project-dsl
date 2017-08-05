e344027b862b60c77c877e23da9eb257c97f9bd7
root <root@openapi-master>
2017-08-05 13:43:52 +0000

    b

--- templates/PiplineRequestTemplate.groovy
+++ templates/PiplineRequestTemplate.groovy
@@ -2,13 +2,24 @@ package templates
 
 class PiplineRequestTemplate {
     static void create(job, config) {
-        pipelineJob('example-pipline1') {
-            logRotator(30, -1, 1, -1)
-            definition {
-                cpsScm {
-                    scm {
-                        git('https://github.com/jenkinsci/job-dsl-plugin.git', '**/master')
-                        scriptPath('jenkinsfile')
+        pipeline {
+            agent any
+            parameters {
+                string(name: 'myInput', description: 'Some pipeline parameters')
+            }
+            stages {
+                stage('Stage one') {
+                    steps {
+                        script {
+                            echo "Parameter from template creation: " + templateParams.someParam
+                        }
+                    }
+                }
+                stage('Stage two') {
+                    steps {
+                        script {
+                            echo "Job input parameter: " + params.myInput
+                        }
                     }
                 }
             }
